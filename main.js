5f85969267ca2d0e75b9eff0a0a8b1976b92766e
liviamarcondes2810 <livia.soboleski.marcondes@escola.pr.gov.br>
2026-05-14 14:31:33 +0000

    Ultima aula, final

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      abas[j].classList.remove("ativo");
     }
 
     botoes[i].classList.add("ativo");
+    abas[i].classList.add("ativo");
   };
 }
+
+// Mostrar primeira aba por padrão
+abas[0].classList.add("ativo");
